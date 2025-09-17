82cb6a46a9e661b36da47e6ea69b4c17a0be2f65
EveKeren <eve.keren6@gmail.com>
2025-09-17 18:37:14 +0300

    first commit

---
+++ users/helpers/bcrypt.js
@@ -0,0 +1,5 @@
+import bcrypt from "bcryptjs";
+const generatePassword = (password) => bcrypt.hashSync(password, 10);
+const comparePassword = (password, hashPassword) =>
+  bcrypt.compareSync(password, hashPassword);
+export { generatePassword, comparePassword };
